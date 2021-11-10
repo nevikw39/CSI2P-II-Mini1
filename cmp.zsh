90f5bf484e75b5c456c797b15a8b7c9bd0acce4a
nevikw39 <nevikw39@icloud.com>
2021-11-10 17:47:09 +0800

    cmp: Ignore div by 0 & UB

--- cmp.zsh
+++ cmp.zsh
@@ -5,11 +5,12 @@ while read in; do
     input+=$in'\n';
 done;
 main=("${(@f)$(echo $input | time ./main 2> /dev/null | AssemblyCompiler/ASMC)}")
-cling=("${(@f)$(echo "#include <stdio.h>\nint x=2, y=3, z=5;${input}printf(\"x, y, z = %d, %d, %d\\\\n\", x, y, z)" | cling -w 2>&1)}");
-if [[ $cling =~ "error" ]]; then
+cling=("${(@f)$(echo "#include <stdio.h>\nint x=2, y=3, z=5;${input}printf(\"x, y, z = %d, %d, %d\\\\n\", x, y, z)" | cling 2>&1)}");
+if [[ $cling =~ "warning" ]]; then
+    echo "\033[0;33m${cling[6]}\033[m";
+elif [[ $cling =~ "error" ]]; then
     if [[ $main =~ "CE" ]]; then
         echo "\033[1;32mAC\033[m\n\033[0;33mCompile Error\033[m";
-        exit;
     else
         echo "\033[1;31mWA\033[m\n\033[0;33mCompile Error\033[m";
         exit 1;
@@ -17,7 +18,6 @@ if [[ $cling =~ "error" ]]; then
 else
     if [[ $main[1] == $cling[6] ]]; then
         echo "\033[1;32mAC\033[m\n\033[0;37m${main[2]}\033[m";
-        exit;
     else
         echo "\033[1;31mWA\033[m\n${main[1]}\n${cling[6]}";
         exit 1;
