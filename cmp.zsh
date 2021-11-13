a9942390f7832febd48a16978935150e7cc3433c
nevikw39 <nevik.w39@gmail.com>
2021-11-13 11:46:39 +0800

    Add pipefail

--- cmp.zsh
+++ cmp.zsh
@@ -1,10 +1,15 @@
 #!/bin/zsh
 
+set -o pipefail
 input="";
 while read in; do
     input+=$in'\n';
 done;
 optimized=("${(@f)$(echo $input | time ./optimized 2> /dev/null | AssemblyCompiler/ASMC)}")
+if [[ $? == 136 ]]; then
+    echo "\033[0;33mfloating point exception\033[m";
+    exit -1;
+fi
 cling=("${(@f)$(echo "#include <stdio.h>\nint x=2, y=3, z=5;${input}printf(\"x, y, z = %d, %d, %d\\\\n\", x, y, z)" | cling 2>&1)}");
 if [[ $? == 136 ]]; then
     echo "\033[0;33mfloating point exception\033[m";
